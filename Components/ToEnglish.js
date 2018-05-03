d45517b9295bfef1362313d36ee69cef24bc0ded
Shannon <schristinep@gmail.com>
2018-05-02 21:57:19 -0500

    this is a rage commit because after 15 minutes i figured out why correct inputs never match as correct and it's because the text input defaults to having one space character at the beginning before the gd input line starts and i need to stop and acknowledge the senseless absurdity of this situation. ughhhhh

--- Components/ToEnglish.js
+++ Components/ToEnglish.js
@@ -10,6 +10,8 @@ export default class ToEnglish extends Component {
     this.state = {
       currentSentence: '',
       input: '',
+      correct: 0,
+      incorrect: 0,
     };
     this.submitAnswer = this.submitAnswer.bind(this);
     this.randomSentence = this.randomSentence.bind(this);
@@ -28,20 +30,23 @@ export default class ToEnglish extends Component {
   }
 
   updateInput(event) {
-    console.log('on text change')
-    this.setState({ input: event.nativeEvent.text });
+    this.setState({ input: event.nativeEvent.text }, () => {console.log(this.state.input.length)});
   }
 
   // figure out how to account for capitalization in data
   submitAnswer() { // answer is key so check if sentences at input is equal to current sentence
     console.log('answer submitted', this.state.input);
-    // if (this.state.currentSentence.toLowerCase() === sentences['1'][this.state.input.toLowerCase()]) {
-    //   this.setState({ correct: this.state.correct + 1 });
-    //   this.randomSentence();
-    // } else {
-    //   this.setState({ incorrect: this.state.incorrect + 1 });
-    //   this.randomSentence();
-    // }
+    if (this.state.input.toLowerCase() === sentences['1'][this.state.currentSentence]) {
+      console.log('correct answer');
+      this.setState({ correct: this.state.correct + 1 });
+    } else {
+      console.log('incorrect answer');
+      this.setState({ incorrect: this.state.incorrect + 1 });
+    }
+    // console.log(sentences['1'][this.state.currentSentence].length + ' is equal to ' +  this.state.input.toLowerCase().length);
+    // console.log('are these equal?', this.state.input.toLowerCase() === sentences['1'][this.state.currentSentence]);
+    this.randomSentence();
+    this.textInput.clear();
   }
 
   render() {
@@ -50,13 +55,17 @@ export default class ToEnglish extends Component {
             <Text> Hello nerds </Text>
             <Text> {this.state.currentSentence} </Text>
             <TextInput
+                ref={(input) => { this.textInput = input; }}
                 onChange={this.updateInput}
+                // onSubmitEditing={this.on}
             > </TextInput>
             <Button
                 onPress={this.submitAnswer}
                 title="Submit"
                 accessibilityLabel="submit answer"
             ></Button>
+            <Text> Correct: { this.state.correct } </Text>
+            <Text> Incorrect: { this.state.incorrect } </Text>
         </View>
     );
   }
