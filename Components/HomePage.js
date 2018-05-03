089b00ea2237065d6ddf6da165403d4246ade7ac
Shannon <schristinep@gmail.com>
2018-05-03 05:19:45 -0500

    bout to try some weird stuff to make gradient work so i am committing

--- Components/HomePage.js
+++ Components/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, Button, View } from 'react-native';
+import { StyleSheet, Text, Button, View, Keyboard } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 // import About from './About';
 // import WordMatch from './WordMatch';
 
@@ -9,17 +10,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-evenly',
   },
+  linearGradient: {
+    flex: 1,
+    width: '100%',
+  },
   textContainer: {
     flex: 1,
-    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'flex-end',
+    width: '100%',
   },
   buttonContainer: {
     flex: 2,
-    backgroundColor: 'purple',
     alignItems: 'center',
     justifyContent: 'space-evenly',
+    width: '100%',
   },
   button: {
     width: '80%',
@@ -39,51 +44,62 @@ export default class HomePage extends Component {
     title: 'Welcome',
   }
 
+  componentDidMount() {
+      Keyboard.dismiss();
+  }
+
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-            <View style={styles.textContainer}>
-                <Text> Choose an activity: </Text>
+        <LinearGradient colors={['#8CAOD7', '#9D79BC', '#A14DAO']} style={styles.linearGradient}>
+
+        <View style={styles.textContainer}>
+            <Text style={{ color: '#7E1F86' }}> Choose an activity: </Text>
+        </View>
+        </LinearGradient>
+        <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+                <Button
+                    onPress={() => {
+                        navigate('About');
+                    }}
+                    title="About"
+                    color="#91C4F2"
+                    accessibilityLabel="about page"
+                />
             </View>
-            <View style={styles.buttonContainer}>
-                <View style={styles.button}>
-                    <Button
-                        onPress={() => {
-                            navigate('About');
-                        }}
-                        title="About"
-                        accessibilityLabel="about page"
-                    />
-                </View>
-                <View style={styles.button}>
-                    <Button
-                        onPress={() => {
-                            navigate('WordMatch');
-                        }}
-                        title="Match Words"
-                        accessibilityLabel="match words"
-                    />
-                </View>
-                <View style={styles.button}>
-                    <Button
-                        onPress={() => {
-                            navigate('ToEnglish');
-                        }}
-                        title="Translate to English"
-                        accessibilityLabel="translate to english"
-                    />
-                </View>
-                <View style={styles.button}>
-                    <Button
-                        onPress={() => {
-                            navigate('ToElvish');
-                        }}
-                        title="Translate to Elvish"
-                        accessibilityLabel="translate to elvish"
-                    />
-                </View>
+            <View style={styles.button}>
+                <Button
+                    onPress={() => {
+                        navigate('WordMatch');
+                    }}
+                    title="Match Words"
+                    color="#91C4F2"
+                    accessibilityLabel="match words"
+                />
             </View>
+            <View style={styles.button}>
+                <Button
+                    onPress={() => {
+                        navigate('ToEnglish');
+                    }}
+                    title="Translate to English"
+                    color="#91C4F2"
+                    accessibilityLabel="translate to english"
+                />
+            </View>
+            <View style={styles.button}>
+                <Button
+                    onPress={() => {
+                        navigate('ToElvish');
+                    }}
+                    color="#91C4F2"
+                    title="Translate to Elvish"
+                    accessibilityLabel="translate to elvish"
+                />
+            </View>
+        </View>
       </View>
     );
   }
